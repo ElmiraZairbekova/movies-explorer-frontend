d36cc00fdc7b8409d4639f9bcec29a627f720171
ElmiraZairbekova <loveni-shop@yandex.ru>
2023-02-06 20:15:27 +0300

    add useHistory

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const PageNotFound = () => {
+  const history = useHistory();
   return (
     <section className="not-found">
       <h2 className="not-found__code">404</h2>
       <p className="not-found__message">Страница не найдена</p>
-      <Link to="/" className="not-found__back">
+      <p onClick={history.goBack} className="not-found__back">
         Назад
-      </Link>
+      </p>
     </section>
   );
 };
